46e9887c7fd8ec67ac29618f48a9fc2278e73a44
Sasha V <sashav4@gmail.com>
2017-11-12 01:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet (integer){
-  if (integer()(<=400) {
+function scuberGreetingForFeet (){
+  if (scuberGreetingForFeet(<=400) {
     return ('This one is on me!')
   }
 }
