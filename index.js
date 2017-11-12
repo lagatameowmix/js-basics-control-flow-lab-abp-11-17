c1f1fda82f87df3ff9d3d278edda8460558d78e1
Sasha V <sashav4@gmail.com>
2017-11-12 02:35:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function switchOnCharmFromTip(tip){
     return "Thank you so much."
   case 'not as generous':
     return "Thank you."
-  case 'anything else':
+  case 'undefined':
     return "Bye."
   }
 }
